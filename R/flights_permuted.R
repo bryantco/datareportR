1a7e86315f45c4db02d952829c4e7a84633c288c
Cong <bcong@crai.com>
2025-09-04 14:07:34 -0500

    feat: add documentation of permuted flights data in R

---
+++ R/flights_permuted.R
@@ -0,0 +1,7 @@
+#' Permuted nycflights13 data
+#' 
+#' @format permuted flights data. Source data is the data from the nycflights13 package.
+#' The data is included to test the diffdf feature of the package, which detects
+#' differences between two datasets.
+#' See commmit 41423e5510c4716a4b70fada1286c85dd698390a for the source script used
+#' to generate the data.
