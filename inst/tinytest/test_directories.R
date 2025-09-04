491d82bb85da3ac311725509171cc4f1da22d6be
Cong <bcong@crai.com>
2025-09-04 15:38:18 -0500

    feat: add unit test for saving to output dir

--- inst/tinytest/test_directories.R
+++ inst/tinytest/test_directories.R
@@ -8,7 +8,20 @@ render_data_report(
   df_input = flights,
   save_report_to_disk = TRUE,
   df_input_old = flights_permuted,
-  save_rmd_dir = "../../data/"
+  save_rmd_dir = "../../data/",
+  save_html_dir = "../../data/"
+)
+
+# Try rendering for a directory that doesn't exist
+expect_error(
+  render_data_report(
+    df_input = flights,
+    save_report_to_disk = TRUE,
+    df_input_old = flights_permuted,
+    save_rmd_dir = "../../data/",
+    save_html_dir = "output/"
+  ),
+  pattern = "Directory to save output HTML"
 )
 
 expect_true(file.exists("../../data/data_report.Rmd"))
