a1d3ba3597728f2401fa1db38215636f14031ad5
Cong <bcong@crai.com>
2025-09-04 15:18:03 -0500

    feat: update directory test with Rmd

--- inst/tinytest/test_directories.R
+++ inst/tinytest/test_directories.R
@@ -7,8 +7,14 @@ flights_permuted = datareportR::flights_permuted
 render_data_report(
   df_input = flights,
   save_report_to_disk = TRUE,
-  save_rmd_dir = getwd(),
+  save_rmd_dir = "../../data/",
   df_input_old = flights_permuted
 )
 
-expect_true(file.exists("data_report.Rmd"))
+expect_true(file.exists("../../data/data_report.Rmd"))
+
+report_rmd_removed = file.remove("../../data/data_report.Rmd")
+expect_true(report_rmd_removed)
+
+report_html_removed = file.remove("../../data/data_report.html")
+expect_true(report_html_removed)
