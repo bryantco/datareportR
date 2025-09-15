622a997f799f3f960bcd11a9b94cc0e648b7f6c6
Cong <bcong@crai.com>
2025-09-15 17:00:26 -0500

    fix: fix html dir to save report in testing data report

--- inst/tinytest/test_data_report.R
+++ inst/tinytest/test_data_report.R
@@ -10,7 +10,7 @@ datareportR::render_data_report(
   save_report_to_disk = TRUE,
   df_input_old = flights_permuted,
   save_rmd_dir = getwd(),
-  save_html_dir = "../../output/",
+  save_html_dir = getwd(),
   include_skim = TRUE,
   include_diffdf = TRUE
 )
